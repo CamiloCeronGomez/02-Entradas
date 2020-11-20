8be7785c5940a1bcd26205e7bd746e49bcc911ed
Camilo Ceron <camiloceron1990@gmail.com>
2020-11-20 16:49:53 -0500

    se crea nuevamnte el repositorio con los cambios, debido a problemas en los commit

---
+++ contenido.js
@@ -0,0 +1,4 @@
+//prompt saca una ventana flotante en la pantalla donde puedo colocar el texto que se le asigna a la variable nombre
+var nombre = prompt("Dime tu nombre");
+// el + es para concatenar el texto con el valor de nombre que se asigna a partir del prompt
+alert("Hola," + nombre);
